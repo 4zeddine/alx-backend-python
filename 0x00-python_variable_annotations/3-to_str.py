6708fa4811aba93da5a1d54bb6dc7ade76b4fa77
4zeddine <azeddine.nader@gmail.com>
2024-08-01 15:56:27 +0100

    Task 3 commit

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Task 3 module."""
+
+
+def to_str(n: float) -> str:
+    """Convert a float to a string.
+
+    Args:
+        n (float): The float to convert to a string.
+
+    Returns:
+        str: The string representation of the float.
+    """
+    return str(n)
