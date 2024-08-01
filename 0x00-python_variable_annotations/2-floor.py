054ac4496c748b75aa74883bde9a105f02c79523
4zeddine <azeddine.nader@gmail.com>
2024-08-01 15:52:59 +0100

    Task 2 commit

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Task 2 module"""
+
+
+def floor(n: float) -> int:
+    """
+    Rounds down a float to the nearest integer.
+
+    Args:
+        n (float): The float to be rounded down.
+
+    Returns:
+        int: The rounded down integer.
+    """
+    return int(n)
