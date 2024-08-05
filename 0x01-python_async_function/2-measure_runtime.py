21feef4ca17e0e805e099f72a91e76129a12fa2a
4zeddine <azeddine.nader@gmail.com>
2024-08-05 22:41:00 +0100

    Task 2 commit

---
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+"""Task 2. Measure the runtime"""
+import time
+import asyncio
+
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+
+def measure_time(n: int, max_delay: int) -> float:
+    """Return the average runtime of wait_n(n, delay)"""
+    start_time = time.time()
+    asyncio.run(wait_n(n, max_delay))
+    return (time.time() - start_time) / n
