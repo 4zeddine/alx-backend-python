0fa1dc8b199c659cd34d8ef14002c8e9d4f4a411
4zeddine <azeddine.nader@gmail.com>
2024-08-05 22:36:51 +0100

    Task 1 commit

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -3,10 +3,13 @@
 import asyncio
 from typing import List
 
+
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(max_delay: int, n: int) -> List[float]:
-    """Function that returns the list of all the delays"""
-    delays = [wait_random(max_delay) for _ in range(n)]
-    return [await delay for delay in asyncio.as_completed(delays)]
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """Function that returns a list of delays in ascending order"""
+    wait_times = await asyncio.gather(
+        *tuple(map(lambda _: wait_random(max_delay), range(n)))
+    )
+    return sorted(wait_times)
