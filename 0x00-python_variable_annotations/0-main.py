bdea9d624194bbad99e7140bdcb11ac2e54d6509
4zeddine <azeddine.nader@gmail.com>
2024-08-26 21:44:21 +0100

    Tasks for the project, commits

--- 0x00-python_variable_annotations/0-main.py
+++ 0x00-python_variable_annotations/0-main.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-add = __import__('0-add').add
 
-print(add(1.11, 2.22) == 1.11 + 2.22)
-print(add.__annotations__)
+make_multiplier = __import__('8-make_multiplier').make_multiplier
+print(make_multiplier.__annotations__)
+fun = make_multiplier(2.22)
+print("{}".format(fun(2.22)))
